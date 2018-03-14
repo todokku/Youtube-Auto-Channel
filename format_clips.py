c5f9ffbe656a96ceb685528cfbc57920743e956c
Ehren Julien-Neitzert <ehrenjulienneitzert@cmail.carleton.ca>
2018-03-14 13:24:20 -0400

    Update format_clips.py
    Made it so text no longer goes off the side of clips if it's too long

--- format_clips.py
+++ format_clips.py
@@ -1,15 +1,42 @@
+#from PIL import Image
+#import numpy as np
+#import moviepy
 from moviepy.video import fx
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
-import time as t
+
+
+#adds newlines to text so that it is never wider than max_len
+#if possible, it adds newlines in such a way so that words aren't broken in half
+def _add_newlines(text, max_len):
+        new_text = ''
+        spot = 0
+        while spot < len(text):
+            substr = text[spot: spot + max_len]
+            last_space = substr.rfind(' ')
+            if len(substr) == max_len:
+                if last_space != -1:
+                    new_text += substr[:last_space] + '\n'
+                    spot += last_space + 1 #+1 to go 1 past the space
+                else:
+                    new_text += substr + '\n'
+                    spot += max_len
+            else:
+                new_text += substr
+                break
+        if new_text[-1] == '\n':
+            new_text = new_text[:-1]
+        return new_text
+    
 
 # return VideoClip resized, bordered and with text
-def format_clip(clip, text=None, text_pos='bottom', fontsize = 60 , ideal_size = (1920,1080)):
-
+def format_clip(clip, text=None, text_pos='bottom', fontsize = 60 , ideal_size = (1920,1080), max_text_len = 45):
+    
     if type(clip) == str:
         clip = VideoFileClip(clip)
 
     text_height = 0
-    if text != None:
+    if text != None and text_pos != None:
+        text = _add_newlines(text, max_text_len)
         t = TextClip(
                         txt=text,fontsize=fontsize,color='white'
                      ).set_pos(('center',text_pos)).set_duration(clip.duration).set_fps(clip.fps)
@@ -53,6 +80,6 @@ def format_clip(clip, text=None, text_pos='bottom', fontsize = 60 , ideal_size =
         return bordered_clip
     
 if __name__ == "__main__":
-    a = format_clip("input file path goes here",text=None,ideal_size=(1920,1080))
-    a.write_videofile("output file path goes here", verbose=False, progress_bar=False, preset='ultrafast')
- 
+    a = format_clip("C:\\Users\\Monchy\Dropbox\\MEMES\ehren yt\\ewe.gif",text='texting texting 123 HAHAHA')
+    a.write_videofile("C:\\Users\\Monchy\Dropbox\\MEMES\ehren yt\\wew.mp4")
+    
